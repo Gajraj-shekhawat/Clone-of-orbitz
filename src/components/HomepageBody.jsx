f5a4bad2cb9e10402469ae437160d0409de34ea1
Gajraj Singh Shekhawat <gaju2gajraj@gmail.com>
2022-06-14 15:34:51 +0530

    Reward part Done

--- src/components/HomepageBody.jsx
+++ src/components/HomepageBody.jsx
@@ -148,12 +148,15 @@ const HomepageBody = () => {
                     </li>
 
                     <h4>Text yourself a download link</h4>
+                    <div className={styles.download_link}>
                     <select className={styles.country_codes}>
                         <Countries/>
                     </select>
                     <input type="number" className={styles.app_number} placeholder="Phone Number" />
-                    <button className={`${styles.submit} ${styles.get_the_app}`}>Get the app</button>
-                    <p className={styles.alert}></p>
+                    <div className={styles.stays_saerch}>
+                        <button>Get the app</button>
+                        </div>
+                    </div>
                     <p className={styles.message}>
                         By providing your number, you agree to receive a one-time automated
                         text message with a link to get the app. Standard text message rates
